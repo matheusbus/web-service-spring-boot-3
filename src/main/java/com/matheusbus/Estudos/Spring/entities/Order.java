8c5c743f48bd8d3dfdbaf8377a6316a9ee1d4560
matheus <matheusbuschermoehle@gmail.com>
2023-03-12 15:16:26 -0300

    OrderStatus enum

--- src/main/java/com/matheusbus/Estudos/Spring/entities/Order.java
+++ src/main/java/com/matheusbus/Estudos/Spring/entities/Order.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.matheusbus.Estudos.Spring.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -30,6 +31,8 @@ public class Order implements Serializable{
 	@JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -38,9 +41,10 @@ public class Order implements Serializable{
 	
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
+		setOrderStatus(orderStatus);
 		this.moment = moment;
 		this.client = client;
 	}
@@ -69,6 +73,16 @@ public class Order implements Serializable{
 		this.client = client;
 	}
 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}	
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
