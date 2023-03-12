747db070e69cbcfc60b83c7310537fc024534ccc
matheus <matheusbuschermoehle@gmail.com>
2023-03-11 23:11:07 -0300

    Project Created

---
+++ src/test/java/com/matheusbus/Estudos/Spring/EstudosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusbus.Estudos.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
