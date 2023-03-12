30464cfe6ebe6aa6bdd1e1e66d984ae8dc9fb329
matheus <matheusbuschermoehle@gmail.com>
2023-03-12 13:11:43 -0300

    Service layer, component registration

--- src/main/java/com/matheusbus/Estudos/Spring/resources/UserResource.java
+++ src/main/java/com/matheusbus/Estudos/Spring/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.matheusbus.Estudos.Spring.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheusbus.Estudos.Spring.entities.User;
+import com.matheusbus.Estudos.Spring.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		return ResponseEntity.ok().body((new User(1L, "Matheus", "matheusbuschermoehle@gmail.com", "47 991705840", "123456")));
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		return ResponseEntity.ok().body(service.findById(id));
 	}
 	
 }
