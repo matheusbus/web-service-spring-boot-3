a45bfe541259295b04096c5e7ada86397bdaeebd
matheus <matheusbuschermoehle@gmail.com>
2023-03-25 09:22:59 -0300

    ManytoMany Association for Product and Category

--- src/main/java/com/matheusbus/Estudos/Spring/entities/Product.java
+++ src/main/java/com/matheusbus/Estudos/Spring/entities/Product.java
@@ -6,9 +6,13 @@ import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -27,7 +31,10 @@ public class Product implements Serializable{
 	private Double price;
 	private String imgUrl;
 	
-	@Transient
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
