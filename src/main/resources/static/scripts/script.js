f53cc9f10f6d70b3e91e96517907cc093245eda4
matheus <matheusbuschermoehle@gmail.com>
2023-03-25 16:55:32 -0300

    JS file added for tests

---
+++ src/main/resources/static/scripts/script.js
@@ -0,0 +1,11 @@
+function salvarUsuario(){
+    const user = {
+        login : document.getElementById("login").value,
+        password
+    };
+
+    let login = document.getElementById("login").value;
+
+    document.getElementById("apresenta_login").innerHTML = login;
+    
+}
